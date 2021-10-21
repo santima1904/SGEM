01638e4d405e316128def377c5a28be3819f8879
Santi <santima1904@gmail.com>
2021-10-21 14:35:38 +0200

    . net core empezado

---
+++ HelloWorld_MVC_02/HelloWorld_MVC_Cor_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorld_MVC_Cor_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ HelloWorld_MVC_02/HelloWorld_MVC_Core_UI/Controllers/ProductosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloWorld_MVC_Core_UI.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult ListaProductos()
+        {
+            return View();
+        }
+    }
+}
