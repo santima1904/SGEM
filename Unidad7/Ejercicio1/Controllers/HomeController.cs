0e23180bfacf4804be6a11c7dd26ad3468df9a96
Santi <santima1904@gmail.com>
2021-10-26 13:42:39 +0200

    Ejercicio 2 unidad 7 casi acabado

---
+++ Unidad7/Ejercicio1/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Ejercicio1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            clsPersona opersona = new clsPersona("Santi", "Martínez");
+            int hora = DateTime.Now.Hour;
+            string saludo = "Buenos días";
+            if (hora >= 12 && hora < 21)
+            {
+                saludo = "Buenas tardes";
+            }else if(hora >= 21 || hora < 6)
+            {
+                saludo = "Buenas noches";
+            }
+
+            ViewData["saludo"] = saludo;
+            ViewBag.Fecha = DateTime.Now;
+            
+            return View(opersona);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Unidad7/Ejercicio2/Models/DAL/Listado.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ejercicio2.Models;
+
+namespace Ejercicio2.Models
+{
+    public class Listado
+    {
+        List<clsPersona> lista = new List<clsPersona>();
+
+        public List<clsPersona> Lista { get => lista; set => lista = value; }
+
+        /// <summary>
+        /// Método para crear y rellenar una lista con personas
+        /// </summary>
+        /// <returns>lista</returns>
+        public List<clsPersona> crearListadoPersonas()
+        {
+            clsPersona opersona1 = new clsPersona("Santi", "Martínez");
+            clsPersona opersona2 = new clsPersona("Pedro", "Pastor");
+            clsPersona opersona3 = new clsPersona("Jesús", "De los Santos");
+            clsPersona opersona4 = new clsPersona("Albert", "Machío");
+            clsPersona opersona5 = new clsPersona("Sinkem", "Torres");
+
+            lista.Add(opersona1);
+            lista.Add(opersona2);
+            lista.Add(opersona3);
+            lista.Add(opersona4);
+            lista.Add(opersona5);
+
+            return lista;
+        }
+    }
+}
