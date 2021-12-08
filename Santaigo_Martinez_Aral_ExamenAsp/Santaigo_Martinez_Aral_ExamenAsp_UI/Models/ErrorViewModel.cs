481dd7d3f44ad41b5fd30f21eb9b5e3a258642c7
Santi <santima1904@gmail.com>
2021-12-08 19:51:03 +0100

    Suerte en el examen guapeton

---
+++ Santaigo_Martinez_Aral_ExamenAsp/Santaigo_Martinez_Aral_ExamenAsp_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Santaigo_Martinez_Aral_ExamenAsp_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
